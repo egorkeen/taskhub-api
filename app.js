7ca68abf20b486eba483398349939874c5964bfb
Egor Kirienko <egor.keen@mail.ru>
2023-11-28 17:46:10 +0300

    feat: создать ручки добавления todo'шек и получения todo'шек

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-const { DB_URL = 'mongodb://127.0.0.1:27017/taskhubdb' } = process.env;
+const { DB_URL = 'mongodb://127.0.0.1/taskhubdb' } = process.env;
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
