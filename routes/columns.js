ae7b3d12c1e6c1bc83bbccd804c275ba528b164c
Egor Kirienko <egor.keen@mail.ru>
2023-12-02 15:37:04 +0300

    feat: подключить ручку get для получения колонок в роуте

--- routes/columns.js
+++ routes/columns.js
@@ -1,8 +1,9 @@
 const { celebrateCreateColumn } = require("../middlewares/celebrate/celebrate-columns");
-const { createColumn } = require("../controllers/columns");
+const { createColumn, getColumns } = require("../controllers/columns");
 const auth = require('../middlewares/auth');
 const columnsRouter = require('express').Router();
 
 columnsRouter.post('/boards/:boardId/', auth, celebrateCreateColumn, createColumn);
+columnsRouter.get('/boards/:boardId/', auth, getColumns);
 
 module.exports = columnsRouter;
