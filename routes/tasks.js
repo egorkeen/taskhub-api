849c8dc7b7a79891f7719a90755dd869f9ad159c
Egor Kirienko <egor.keen@mail.ru>
2023-12-02 15:02:07 +0300

    feat: переписать путь для post-запроса так, чтобы задачи создавались внутри columns

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,6 @@ const { createTask } = require("../controllers/tasks");
 const { celebrateCreateTask } = require('../middlewares/celebrate/celebrate-tasks');
 const auth = require('../middlewares/auth');
 
-tasksRouter.post('/boards/:boardId/', auth, celebrateCreateTask, createTask);
+tasksRouter.post('/boards/:boardId/:columnId/', auth, celebrateCreateTask, createTask);
 
 module.exports = tasksRouter;
