5099b8932896549be446fdaa881ccf8cb55b9329
Egor Kirienko <egor.keen@mail.ru>
2023-12-02 12:28:25 +0300

    feat: добавить cookieParser для работы с куками

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
 
@@ -9,6 +10,7 @@ const { DB_URL = 'mongodb://127.0.0.1/taskhubdb' } = process.env;
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(routes);
 
 mongoose.connect(DB_URL, { useNewUrlParser: true });
