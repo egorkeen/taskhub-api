2699b7291b08d1d864e038b8715321d62e8fbfd0
Egor Kirienko <egor.keen@mail.ru>
2023-12-02 15:01:22 +0300

    feat: подключить роут columnsRouter

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const userRouter = require('./users');
 const boardsRouter = require('./boards');
+const columnsRouter = require('./columns');
 const tasksRouter = require('./tasks');
 
 router.use(userRouter);
 router.use(boardsRouter);
+router.use(columnsRouter);
 router.use(tasksRouter);
 
 module.exports = router;
