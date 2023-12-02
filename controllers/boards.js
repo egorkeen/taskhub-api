ac1c6d7833fc6a6eaed0343a4ce32fd40e5e8e32
Egor Kirienko <egor.keen@mail.ru>
2023-12-02 14:57:08 +0300

    fix: добавление id board'а в поле пользователя userBoardsIds

--- controllers/boards.js
+++ controllers/boards.js
@@ -5,27 +5,25 @@ const User = require('../models/User');
 
 module.exports.createBoard = async (req, res, next) => {
   try {
-    const {
-      title,
-      description,
-      owner,
-      style,
-      date,
-    } = req.body;
+    const { title, description, owner, style, date } = req.body;
+    const { uid } = req.cookies;
 
+    // id доски
     const id = uuid4();
     const users = [];
-    // users.push(id пользователя)
-    const newBoard = {
-      id,
-      title,
-      description,
-      owner,
-      style,
-      users,
-      date
-    };
+    // добавляем создателя в список пользователей доски
+    users.push(uid);
+
+    // создаем доску
+    const newBoard = { id, title, description, owner, style, users, date };
+
     const result = await setToDatabase(BOARDS, newBoard);
+
+    // после того как мы получили успешно доски, добавляем id доски в поле usersBoardsIds
+    const user = await User.findUserById(uid);
+    user.userBoardsIds.push(id);
+    await user.save();
+
     res.send(result);
   } catch (err) {
     console.log(err);
@@ -39,7 +37,9 @@ module.exports.getUserBoards = async (req, res, next) => {
   const { userBoardsIds = [] } = user;
   const promiseArr = [];
   for (const boardId of userBoardsIds) {
-    promiseArr.push(getFromDatabase(`${BOARDS}/${boardId}`));
+    const board = await getFromDatabase(`${BOARDS}/${boardId}`);
+    console.log(board);
+    promiseArr.push(board);
   };
   const userBoards = await Promise.all(promiseArr);
   res.send(userBoards);
