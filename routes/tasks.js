4e919422a9d480db30f4c307e6700563111d9559
Egor Kirienko <egor.keen@mail.ru>
2023-12-02 15:37:34 +0300

    feat: подключить ручку get для задач, но закомментировать на время

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,9 @@
 const tasksRouter = require('express').Router();
-const { createTask } = require("../controllers/tasks");
+const { createTask, getTasks } = require("../controllers/tasks");
 const { celebrateCreateTask } = require('../middlewares/celebrate/celebrate-tasks');
 const auth = require('../middlewares/auth');
 
-tasksRouter.post('/boards/:boardId/:columnId/', auth, celebrateCreateTask, createTask);
+tasksRouter.post('/boards/:boardId/columns/:columnId/', auth, celebrateCreateTask, createTask);
+// tasksRouter.get('boards/:boardId/columns/:columnId/', auth, getTasks);
 
 module.exports = tasksRouter;
