9ae3ac45cb67f3741a76ea3eb4d84624d19df61d
Egor Kirienko <egor.keen@mail.ru>
2023-12-02 13:12:13 +0300

    feat: подключить tasksRouter

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRouter = require('./users');
 const boardsRouter = require('./boards');
+const tasksRouter = require('./tasks');
 
 router.use(userRouter);
 router.use(boardsRouter);
+router.use(tasksRouter);
 
 module.exports = router;
