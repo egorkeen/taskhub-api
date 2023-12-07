c14c3da952da23a6160ae824f6d81b434db1b956
Egor Kirienko <egor.keen@mail.ru>
2023-12-07 17:59:49 +0300

    feat: улучшить обработку ошибок

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,7 +3,7 @@ const { createTask, getTasks } = require("../controllers/tasks");
 const { celebrateCreateTask } = require('../middlewares/celebrate/celebrate-tasks');
 const auth = require('../middlewares/auth');
 
-tasksRouter.post('/boards/:boardId/columns/:columnId/', auth, celebrateCreateTask, createTask);
-tasksRouter.get('boards/:boardId/columns/:columnId/', auth, getTasks);
+tasksRouter.post('/boards/:boardId/sections/:sectionId/', auth, celebrateCreateTask, createTask);
+tasksRouter.get('boards/:boardId/sections/:sectionId/', auth, getTasks);
 
 module.exports = tasksRouter;
